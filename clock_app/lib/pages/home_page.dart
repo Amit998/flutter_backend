0691367dda3d358f2e0824e7b8695216353d9522
amit998 <damitlucky998@gmail.com>
2021-05-24 19:43:10 +0530

    added

--- clock_app/lib/pages/home_page.dart
+++ clock_app/lib/pages/home_page.dart
@@ -15,49 +15,95 @@ class _MyHomePageState extends State<MyHomePage> {
     var formattedDate = DateFormat('EEE, d MMM').format(now);
     var timeZoneString = now.timeZoneOffset.toString().split('.').first;
 
+    var offsetSign = '';
+
+    if (!timeZoneString.startsWith('-')) {
+      offsetSign = '+';
+    }
+
     print(timeZoneString);
 
     return Scaffold(
       backgroundColor: Color(0xFF2D2F41),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Clock",
-              style: TextStyle(color: Colors.white, fontSize: 24),
+      body: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // ignore: deprecated_member_use
+              FlatButton(
+                  onPressed: () {},
+                  child: Column(
+                    children: [
+                      Image.asset("assets/clock_icon.png"),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "Clock",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      )
+                    ],
+                  )),
+            ],
+          ),
+          VerticalDivider(
+            color: Colors.white,
+            width: 1,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 30),
+              alignment: Alignment.center,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Clock",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    formattedTime.toString(),
+                    style: TextStyle(color: Colors.white, fontSize: 44),
+                  ),
+                  Text(
+                    formattedDate.toString(),
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  ClockView(),
+                  Text(
+                    "Timezone",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.language,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Text(
+                        "UTC" + offsetSign + timeZoneString,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            letterSpacing: 1.5),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-            Text(
-              "Clock",
-              style: TextStyle(color: Colors.white, fontSize: 44),
-            ),
-            Text(
-              "Clock",
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            ClockView(),
-            Text(
-              "TimeZone",
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.language,
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  "UTC",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                )
-              ],
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
