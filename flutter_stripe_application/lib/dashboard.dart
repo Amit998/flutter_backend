2569df64008a8149d31b898a0026af6a981bce9f
amit998 <damitlucky998@gmail.com>
2021-08-18 10:07:00 +0530

    added

--- flutter_stripe_application/lib/dashboard.dart
+++ flutter_stripe_application/lib/dashboard.dart
@@ -1,17 +1,70 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class DashboardPage extends StatefulWidget {
-  const DashboardPage({ Key? key }) : super(key: key);
+  const DashboardPage({Key? key}) : super(key: key);
 
   @override
   _DashboardPageState createState() => _DashboardPageState();
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  Map<String, dynamic>? paymentIntentData;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            makePayMent();
+          },
+          child: Text("Pay Amout"),
+        ),
+      ),
     );
+  }
+
+  Future<void> makePayMent() async {
+    final url = Uri.parse("");
+
+    final response =
+        await http.get(url, headers: {'Content-Type': 'application/json'});
+
+    paymentIntentData = jsonDecode(response.body);
+
+    await Stripe.instance.initPaymentSheet(
+        paymentSheetParameters: SetupPaymentSheetParameters(
+      paymentIntentClientSecret: paymentIntentData!['paymentIntent'],
+      applePay: true,
+      googlePay: true,
+      style: ThemeMode.dark,
+      merchantCountryCode: 'US',
+      merchantDisplayName: 'Amit',
+    ));
+
+    setState(() {});
+
+    displayPaymentSheet();
+  }
+
+  Future<void> displayPaymentSheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet(
+          parameters: PresentPaymentSheetParameters(
+              clientSecret: paymentIntentData!['paymentIntent'],
+              confirmPayment: true));
+
+      setState(() {
+        paymentIntentData = null;
+      });
+
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Paid Successfully")));
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- flutter_stripe_application/lib/main.dart
+++ flutter_stripe_application/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:flutter_stripe_application/cred.dart';
 import 'package:flutter_stripe_application/dashboard.dart';
 
 void main() {
+  Stripe.publishableKey = api_key;
   runApp(MyApp());
 }
 
@@ -10,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
